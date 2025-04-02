0ed8e12a053295c5685f42f5a18041b634dd7157
IraquianRodrigues <iraquianrodrigues@hotmail.com>
2025-04-02 14:57:13 -0300

    finalizando o hero

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,6 +1,59 @@
+"use client"
+
 import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button";
+import { Menu, LogIn } from "lucide-react";
+import { useState } from "react";
 
 export function Header(){
+
+  // condicional para saber se o usuario esta logado ou não
+  const session = null
+
+  // state do model aberto ou fechado
+  const [isOpen, setIsOpen] = useState(false)
+
+  const navItems = [
+    {href: "#profissionais", label: "Profissionais"},
+    {href: "/contatos", label: "Contatos"}
+  ]
+
+  const NavLinks = () => (
+    <>
+    {navItems.map((item) =>(
+      <Button key={item.href} asChild className="bg-transparent hover:bg-transparent text-black shadow-none"
+      onClick={() => setIsOpen(false)}
+      >
+        <Link href={item.href} className="text-base">
+          {item.label}
+        </Link>
+      </Button>
+    ))}
+
+
+    {session ? (
+      <Link href="/dashboard"
+      className="flex items-center justify-center gap-2"
+      >
+       Acessar clinica
+      </Link>
+    ): (
+      <Button>
+        <LogIn/>
+        Portal da clinica
+      </Button>
+    )}
+    </>
+  )
+
     return(
         <header className="fixed top-0 right-0 left-0 z-[999] py-4 px-6 bg-white">
            
@@ -13,9 +66,35 @@ export function Header(){
               Odonto<span className="text-emerald-500">PRO</span>
               </Link>
 
-            <nav>
-              <a href="#">Profissionais</a>
+            <nav className="hidden md:flex items-center space-x-4">
+              <NavLinks/>
             </nav>
+
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild className="md:hidden">
+                <Button className="text-black hover:bg-transparent"
+                variant="ghost"
+                size="icon"
+                >
+                  <Menu className="w-6 h-6"/>
+                </Button>
+              </SheetTrigger>
+
+            <SheetContent side="right" className="w-[240px] sm:w-[300px] z-[9999]">
+              <SheetTitle>Menu</SheetTitle>
+              <SheetHeader></SheetHeader>
+             
+              <SheetDescription>
+              Veja nossos links
+              </SheetDescription>
+              
+              <nav className="flex flex-col space-y-4 mt-6">
+                <NavLinks/>
+              </nav>
+
+            </SheetContent>
+
+            </Sheet>
          
             </div>
         </header>
